7b5c37614399e771630f6bafa4df31fb60e2997b
Eslley <rubemeslley@gmail.com>
2022-10-28 19:54:07 -0300

    #5 Exibindo destinos de interesse no mapa

---
+++ src/components/Map.js
@@ -0,0 +1,68 @@
+import { Box, Modal } from "@mui/material"
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    maxHeight: '100%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 1,
+};
+
+function Map({ open, setOpen, user }) {
+
+    const handleClose = () => setOpen(false)
+    const [marks, setMakers] = []
+
+    useEffect(() => {
+        console.log()
+        // for (let [key, value] of Object.entries(user.places)) {
+        //     console.log(key, value);
+        // }
+    }, [user])
+
+
+    return (
+        <>
+            {user &&
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <MapContainer
+                            style={{
+                                width: '100%',
+                                height: '80vh'
+                            }} center={[20, 0]} zoom={2} scrollWheelZoom={false}>
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            {Object.entries(user.places).map((item) => (
+                                item[1].cities.map((city) => (
+                                    <Marker position={[city.lat, city.log]}>
+                                        <Popup>
+                                            {city.value.split(',')[0]} <br /> {item[1].name}
+                                        </Popup>
+                                    </Marker>
+                                ))
+                            ))}
+
+                        </MapContainer>
+                    </Box>
+                </Modal>
+            }
+        </>
+    )
+}
+
+export default Map
--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,21 +1,24 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Box, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import { useEffect, useState } from "react"
+import Map from "./Map"
 
 function TableUsers() {
 
     const [users, setUsers] = useState([])
+    const [indexUser, setIndexUser] = useState()
+    const [open, setOpen] = useState(false)
 
     useEffect(() => {
         const localData = JSON.parse(localStorage.getItem("users"))
 
         if (localData.length > 0) {
             setUsers(localData)
-            console.log(localData)
         }
     }, [])
 
-    function openModal(user) {
-        console.log(user)
+    function openModal(index) {
+        setOpen(true)
+        setIndexUser(index)
     }
 
     return (
@@ -35,7 +38,7 @@ function TableUsers() {
                                 <TableRow
                                     sx={{ ":hover": { backgroundColor: "rgba(51, 153, 255, 0.2)" }, cursor: "pointer" }}
                                     key={index}
-                                    onClick={() => openModal(user)}>
+                                    onClick={() => openModal(index)}>
                                     <TableCell component="th" scope="row">
                                         {user.name}
                                     </TableCell>
@@ -51,6 +54,9 @@ function TableUsers() {
                     </Table>
                 </TableContainer>
                 : <p>Não há usuários cadastrados!</p>}
+
+                <Map user={users[indexUser]} open={open} setOpen={setOpen} />
+
         </>
     )
 }
